52d7bfb8d4a48c62e09238f31eaced773e230c3f
Sheelu <sheeluy420@gmail.com>
2024-05-20 16:35:29 +0530

    updated

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+<!-- Log on to codeastro.com for more projects! -->
+     ©  {{date('Y')}} Millionaire Track Attendance Management<span class="d-none d-sm-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Sheelu Yadav</span>.
+</footer>
